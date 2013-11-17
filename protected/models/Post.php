7daddcbb54224d059f8f703c2d9d1c105e84d99a
pyxyzn <pyxyzn@127.0.0.1>
2013-11-18 00:07:34 +0800

    修改验证，未完成。

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -35,10 +35,12 @@ class Post extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('title, content, status, author_id', 'required'),
-			array('status, create_time, update_time, author_id', 'numerical', 'integerOnly'=>true),
+			array('title, content, status', 'required'),
 			array('title', 'length', 'max'=>128),
+			array('status', 'in', 'range'=>array(1,2,3)),
 			array('tags', 'safe'),
+			array('tags', 'match', 'pattern'=>'/^[\w\s,]+$/','message'=>'Tags can only contain word characters.'),
+			array('tags', 'normalizeTags'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
 			array('id, title, content, tags, status, create_time, update_time, author_id', 'safe', 'on'=>'search'),
