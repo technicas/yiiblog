3df4e8d5dac251483757b4e06735818e532b2a23
pyxyzn <pyxyzn@127.0.0.1>
2013-11-23 12:30:40 +0800

    test1

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../../../../../../Users/pyxyzn/yii-1.1.14/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
